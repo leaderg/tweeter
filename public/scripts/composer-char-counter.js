b8eb36e764d92c1eb63be5b2ca8443a29ca8b024
leaderg <leader.g@gmail.com>
2019-05-16 19:55:20 +0000

    Primary MongoDB Setup.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,17 +6,4 @@ $("textarea").keyup(function(){
   counter.css((i <= 140) ? {color: "black"} : {color: "red"})
 });
 
-//css stylesheet and just add classes
-
-// $("textarea").keyup(function(){
-//   i = $(this).val().length;
-//   let counter = $(this).siblings().last().text(140 - i);
-//   if (i > 140) {
-//     counter.css({color: "red"})
-//   } else {
-//     counter.css({color: "black"})
-//   }
-// });
-
-
 });
