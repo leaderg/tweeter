00cf1294688e48a06913eec52573df823c1cdd53
leaderg <leader.g@gmail.com>
2019-05-17 18:29:50 +0000

    Added error handling for empty tweets. Added timestamp to tweets.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,13 +6,26 @@
 
 $(document).ready(function() {
 
+function daysAgo(input) {
+  input = Number(input);
+  const currentTime = new Date();
+  let timeAgo = currentTime - input;
+  let days = Math.floor(((timeAgo / (1000*60*60*24*365)) - Math.floor(timeAgo / (1000*60*60*24*365))) * 365);
+  let years = Math.floor(timeAgo / (1000*60*60*24*365));
+  if (years > 0) {
+    return `${years} years and ${days} days ago`;
+  } else {
+    return `${days} days ago`;
+  }
+}
+
 function createTweetElement(tweetObj) {
 
-  let avatar = tweetObj.user.avatars.small
-  let name = tweetObj.user.name
+  let avatar = tweetObj.user.avatars.small;
+  let name = tweetObj.user.name;
   let handle = tweetObj.user.handle;
-  let content = tweetObj.content.text
-  let timestamp = tweetObj.created_at
+  let content = tweetObj.content.text;
+  let timestamp = daysAgo(tweetObj.created_at);
 
   let $tweet = $("<article>").addClass("tweet");
   let $img = $("<img>").addClass("logo").attr("src", avatar);
@@ -45,7 +58,13 @@ function reloadTweets() {
 
 $(".tweetForm").submit(function (event) {
   event.preventDefault();
-  if ($('textarea').val().length <= 140) {
+  if ($('textarea').val().length > 140) {
+    $(".error-text").text("You can't Tweeter a Tweet more than 140 characters. It's just not allowed...")
+    $("#error").show();
+  } else if ($('textarea').val().length < 1) {
+      $(".error-text").text("Can't tweeter an empty tweet.")
+      $("#error").show();
+  } else {
     let $form = $(this),
     tweet = $form.find("textarea[name='text']").val(),
     url = $form.attr("action");
@@ -53,8 +72,6 @@ $(".tweetForm").submit(function (event) {
       $("textarea").val("").trigger('keyup');
       reloadTweets();
     });
-    } else {
-      $("#error").show();
     }
 });
 
